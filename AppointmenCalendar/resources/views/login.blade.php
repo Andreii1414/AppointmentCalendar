59c5f4f4067050a084550c7c15fb2867b08ade0e
Andreii1414 <95772732+Andreii1414@users.noreply.github.com>
2023-07-16 15:02:21 +0300

    front-end

--- AppointmenCalendar/resources/views/login.blade.php
+++ AppointmenCalendar/resources/views/login.blade.php
@@ -4,36 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="{{ url('css/forms.css') }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-    <h1>Login form</h1>
-       <form action="/login" method="POST">
-       @csrf
+    <div class='content'>
+        <h1>Login form</h1>
+        <form action="/login" method="POST">
+        @csrf
 
+                <div>
+                    <input placeholder='Email' type="email" id="email" name="email" required>
+                </div>
+                
+                <div>
+                    <input placeholder='Password' type="password" id="password" name="password" required>
+                </div>
+
+
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+        </form>
+
+        @if ($errors->any())
             <div>
-                <label for="email">Email: </label>
-                <input type="email" id="email" name="email" required>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-            
-            <div>
-                <label for="password">Password: </label>
-                <input type="password" id="password" name="password" required>
-            </div>
-
-
-            <div>
-                <button type="submit">Login</button>
-            </div>
-       </form>
-
-       @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
+            @endif
+    </div>
 </body>
 </html>
--- AppointmenCalendar/resources/views/register.blade.php
+++ AppointmenCalendar/resources/views/register.blade.php
@@ -4,44 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="{{ url('css/forms.css') }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-    <h1>Register form</h1>
-       <form action="/register" method="POST">
-       @csrf
-            <div>
-                <label for="name">Name: </label>
-                <input type="text" id="name" name="name" required>
-            </div>
+    <div class='content'>
+        <h1>Register form</h1>
+        <form action="/register" method="POST">
+        @csrf
+                <div>
+                    <input placeholder='Name' type="text" id="name" name="name" required>
+                </div>
 
-            <div>
-                <label for="email">Email: </label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            
-            <div>
-                <label for="password">Password: </label>
-                <input type="password" id="password" name="password" required>
-            </div>
+                <div>
+                    <input placeholder='Email' type="email" id="email" name="email" required>
+                </div>
+                
+                <div>
+                    <input placeholder='Password' type="password" id="password" name="password" required>
+                </div>
 
-            <div>
-                <label for="repeat">Repeat your password: </label>
-                <input type="password" id="repeat" name="repeat" required>
-            </div>
+                <div>
+                    <input placeholder='Repeat your password' type="password" id="repeat" name="repeat" required>
+                </div>
 
-            <div>
-                <button type="submit">Register</button>
-            </div>
-       </form>
+                <div>
+                    <button type="submit">Register</button>
+                </div>
+        </form>
 
-       @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
+        @if ($errors->any())
+            <div>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+    </div>
 </body>
 </html>
