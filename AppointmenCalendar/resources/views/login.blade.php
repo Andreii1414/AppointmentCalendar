9cc20cf7914f138f5ae6a673807e051d501c2b9a
Andreii1414 <95772732+Andreii1414@users.noreply.github.com>
2023-07-17 12:45:21 +0300

    password visibility + session storage

--- AppointmenCalendar/resources/views/login.blade.php
+++ AppointmenCalendar/resources/views/login.blade.php
@@ -6,19 +6,24 @@
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="{{ url('css/forms.css') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div class='content'>
-        <h1>Login form</h1>
+        <h1> <i class="fa fa-calendar icon"></i>Login form</h1>
         <form action="/login" method="POST">
         @csrf
-
+            
                 <div>
+                    <i class="fa fa-envelope icon"></i>
                     <input placeholder='Email' type="email" id="email" name="email" required>
+                    <i class="fa fa-envelope icon"></i>
                 </div>
                 
                 <div>
+                    <i class="fa fa-key icon"></i>
                     <input placeholder='Password' type="password" id="password" name="password" required>
+                    <i class="fa fa-eye icon" id="showPassword" onclick="passwordVisibility()"></i>
                 </div>
 
                 <div>
@@ -40,5 +45,36 @@
             </div>
             @endif
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function(){
+        var inputEmail = document.getElementById('email');
+        var inputPassword = document.getElementById('password');
+
+        inputEmail.value = sessionStorage.getItem('email');
+        inputPassword.value = sessionStorage.getItem('password');
+
+        inputEmail.addEventListener('input', function(){
+            sessionStorage.setItem('email', inputEmail.value);
+        });
+        inputPassword.addEventListener('input', function(){
+            sessionStorage.setItem('password', inputPassword.value);
+        });
+
+    })
+
+    function passwordVisibility(){
+        var passwordInput = document.getElementById("password");
+        var toggle = document.getElementById("showPassword");
+
+        if(passwordInput.type == "password")
+        {
+            passwordInput.type = "text";
+        }
+        else {
+            passwordInput.type = "password";
+        }
+     }
+    </script>
 </body>
 </html>
--- AppointmenCalendar/resources/views/register.blade.php
+++ AppointmenCalendar/resources/views/register.blade.php
@@ -7,26 +7,35 @@
     <link rel="stylesheet" type="text/css" href="{{ url('css/forms.css') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div class='content'>
-        <h1>Register form</h1>
+        <h1><i class="fa fa-calendar icon"></i>Register form</h1>
         <form action="/register" method="POST">
         @csrf
                 <div>
+                    <i class="fa fa-user icon"></i>
                     <input placeholder='Name' type="text" id="name" name="name" required>
+                    <i class="fa fa-user icon"></i>
                 </div>
 
                 <div>
+                    <i class="fa fa-envelope icon"></i>
                     <input placeholder='Email' type="email" id="email" name="email" required>
+                    <i class="fa fa-envelope icon"></i>
                 </div>
                 
                 <div>
+                    <i class="fa fa-key icon"></i>
                     <input placeholder='Password' type="password" id="password" name="password" required>
+                    <i class="fa fa-eye icon" id="showPassword" onclick="passwordVisibility(1)"></i>
                 </div>
 
                 <div>
+                    <i class="fa fa-key icon"></i>
                     <input placeholder='Repeat your password' type="password" id="repeat" name="repeat" required>
+                    <i class="fa fa-eye icon" id="showPassword" onclick="passwordVisibility(2)"></i>
                 </div>
 
                 <div class="g-recaptcha" data-sitekey="6LdqJy4nAAAAADUUjiZjfSotSUxMikRhkhkQpfOb"></div>
@@ -50,5 +59,49 @@
             </div>
             @endif
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function(){
+            var inputNume = document.getElementById('name');
+            var inputEmail = document.getElementById('email');
+            var inputParola = document.getElementById('password');
+            var inputResparola = document.getElementById('repeat');
+
+            inputNume.value = sessionStorage.getItem('name');
+            inputEmail.value = sessionStorage.getItem('email');
+            inputParola.value = sessionStorage.getItem('password');
+            inputResparola.value = sessionStorage.getItem('repeat');
+
+            inputNume.addEventListener('input', function(){
+                sessionStorage.setItem('name', inputNume.value);
+            });
+            inputEmail.addEventListener('input', function(){
+                sessionStorage.setItem('email', inputEmail.value);
+            });
+            inputParola.addEventListener('input', function(){
+                sessionStorage.setItem('password', inputParola.value);
+            });
+            inputResparola.addEventListener('input', function(){
+                sessionStorage.setItem('repeat', inputResparola.value);
+            })
+
+    })
+
+                        
+    function passwordVisibility(pas){
+        if(pas == 1)
+            var passwordInput = document.getElementById("password");
+        else var passwordInput = document.getElementById("repeat");
+        var toggle = document.getElementById("showPassword");
+
+        if(passwordInput.type == "password")
+        {
+            passwordInput.type = "text";
+        }
+        else {
+            passwordInput.type = "password";
+        }
+     }
+    </script>
 </body>
 </html>
